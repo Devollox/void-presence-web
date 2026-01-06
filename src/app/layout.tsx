6e8ba33dd5797d65d5f0d8c06319a5f9385f341f
Devollox <anisin.97@bk.ru>
2026-01-06 12:23:34 +0300

    chore: add favicons to public

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,22 +35,85 @@ export const metadata: Metadata = {
 	manifest: '/favicons/manifest.json',
 	icons: {
 		icon: [
-			{ url: '/favicon.ico', sizes: '32x32', type: 'image/x-icon' },
 			{
-				url: '/favicons/web-app-manifest-192x192.png',
-				sizes: '192x192',
+				rel: 'icon',
 				type: 'image/png',
+				sizes: '16x16',
+				url: '/favicons/favicon-16x16.png',
+			},
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '32x32',
+				url: '/favicons/favicon-32x32.png',
+			},
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '96x96',
+				url: '/favicons/favicon-96x96.png',
+			},
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '192x192',
+				url: '/favicons/android-icon-192x192.png',
+			},
+			{
+				rel: 'icon',
+				type: 'image/x-icon',
+				sizes: '32x32',
+				url: '/favicons/favicon.ico',
 			},
 		],
 		apple: [
 			{
-				url: '/favicons/web-app-manifest-192x192.png',
-				sizes: '192x192',
-				type: 'image/png',
+				rel: 'apple-touch-icon',
+				sizes: '57x57',
+				url: '/favicons/apple-icon-57x57.png',
+			},
+			{
+				rel: 'apple-touch-icon',
+				sizes: '60x60',
+				url: '/favicons/apple-icon-60x60.png',
+			},
+			{
+				rel: 'apple-touch-icon',
+				sizes: '72x72',
+				url: '/favicons/apple-icon-72x72.png',
+			},
+			{
+				rel: 'apple-touch-icon',
+				sizes: '76x76',
+				url: '/favicons/apple-icon-76x76.png',
+			},
+			{
+				rel: 'apple-touch-icon',
+				sizes: '114x114',
+				url: '/favicons/apple-icon-114x114.png',
+			},
+			{
+				rel: 'apple-touch-icon',
+				sizes: '120x120',
+				url: '/favicons/apple-icon-120x120.png',
+			},
+			{
+				rel: 'apple-touch-icon',
+				sizes: '144x144',
+				url: '/favicons/apple-icon-144x144.png',
+			},
+			{
+				rel: 'apple-touch-icon',
+				sizes: '152x152',
+				url: '/favicons/apple-icon-152x152.png',
+			},
+			{
+				rel: 'apple-touch-icon',
+				sizes: '180x180',
+				url: '/favicons/apple-icon-180x180.png',
 			},
 		],
 	},
-
 	openGraph: {
 		type: 'website',
 		url: siteUrl,
